145d3b6f3b92abece5f5ad7ac99a74f0b334aba2
cavoixanh <thett01337@hotmail.com>
2013-06-05 14:56:37 +0700

    TDD
    test7

--- StringCalculator5_6_2013/StringCalculator/StringCalculator.m
+++ StringCalculator5_6_2013/StringCalculator/StringCalculator.m
@@ -17,9 +17,29 @@
     NSInteger count;
     count = 0;
     addString = [addString stringByReplacingOccurrencesOfString:@"\n" withString:@","];
+    NSString *temp1;
+    if(addString.length >7)
+        temp1 = [addString substringToIndex: 7];
     
+    if([temp1 hasPrefix:@"//["]){
+        temp1 = [temp1 stringByReplacingOccurrencesOfString:@"[" withString:@""];
+        temp1 = [temp1 stringByReplacingOccurrencesOfString:@"]" withString:@""];
+        NSString *key = [temp1 stringByReplacingOccurrencesOfString:@"//" withString:@""];
+        
+        addString = [addString substringFromIndex:8];
+        addString = [addString stringByReplacingOccurrencesOfString:key withString:@","];
+        NSArray *arr = [addString componentsSeparatedByString:@","];
+        for(NSString *string in arr){
+            if([string integerValue] >0)
+                count = count + [string integerValue];
+            
+        }
+        return count;
+    }
     NSString *temp = [addString substringToIndex:3];
+    
     if([temp hasPrefix:@"//"]){
+        
         NSString *key = [temp stringByReplacingOccurrencesOfString:@"//" withString:@""];
         addString = [addString substringFromIndex:2];
         addString = [addString stringByReplacingOccurrencesOfString:key withString:@","];
