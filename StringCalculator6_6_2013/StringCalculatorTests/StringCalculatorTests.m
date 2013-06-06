91e23843b39566f3cd3fb74b5501af2f01ec78f6
cavoixanh <thett01337@hotmail.com>
2013-06-06 14:11:01 +0700

    TDD
    test4

--- StringCalculator6_6_2013/StringCalculatorTests/StringCalculatorTests.m
+++ StringCalculator6_6_2013/StringCalculatorTests/StringCalculatorTests.m
@@ -32,6 +32,7 @@
     STAssertEquals([a Add:@"1,2,3"], 6, @"nhieu so so");
     STAssertEquals([a Add:@"1\n2,3"], 6, @"\n");
     STAssertEquals([a Add:@"//;\n1;2"], 3, @"delemeter");
+    STAssertEquals([a Add:@"//#\n1#2"], 3, @"delemeter");
     
     STAssertThrows([a Add:@"-1,2"], @"negatives not allowed");
 }
