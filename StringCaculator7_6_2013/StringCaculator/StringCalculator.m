f7a30211338078da310e4d525ee5a82efc08a48f
cavoixanh <thett01337@hotmail.com>
2013-06-07 14:01:18 +0700

    TDD
    test2

--- StringCaculator7_6_2013/StringCaculator/StringCalculator.m
+++ StringCaculator7_6_2013/StringCaculator/StringCalculator.m
@@ -11,9 +11,20 @@
 @implementation StringCalculator
 
 - (NSInteger) Add:(NSString*) strAdd{
+    
+    
+    NSInteger count;
+    count = 0;
     if(strAdd.length == 0)
         return 0;
-    
+    else{
+        NSArray *arr = [strAdd componentsSeparatedByString:@","];
+        for(NSString *str in arr){
+            count = count + [str integerValue];
+        }
+            
+        return count;
+    }
     return 0;
 }
 @end
