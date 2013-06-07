0612ed051947836e6cbd3a276eaab8609349eb69
cavoixanh <thett01337@hotmail.com>
2013-06-07 13:49:31 +0700

    TDD
    TDD

---
+++ StringCaculator7_6_2013/StringCaculatorTests/StringCaculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  StringCaculatorTests.h
+//  StringCaculatorTests
+//
+//  Created by Truong Tran  The on 6/7/13.
+//  Copyright (c) 2013 Thett. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface StringCaculatorTests : SenTestCase
+
+@end
---
+++ StringCaculator7_6_2013/StringCaculatorTests/StringCaculatorTests.m
@@ -0,0 +1,32 @@
+//
+//  StringCaculatorTests.m
+//  StringCaculatorTests
+//
+//  Created by Truong Tran  The on 6/7/13.
+//  Copyright (c) 2013 Thett. All rights reserved.
+//
+
+#import "StringCaculatorTests.h"
+
+@implementation StringCaculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in StringCaculatorTests");
+}
+
+@end
